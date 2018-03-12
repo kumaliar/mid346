9becaa0eb5a6c90ddd7410edbb3a50f0f24d11ff
kumaliar <32179094+kumaliar@users.noreply.github.com>
2018-03-12 15:56:17 -0500

    Add files via upload

--- mid/src/com/company/Main.java
+++ mid/src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
+
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.sql.DriverManager;
 
@@ -28,24 +32,53 @@ public class Main {
     }
 
 
-    public static void getCD(String web) throws Exception{
-        ArrayList<Section> classes= new ArrayList<Section>();
-        Section class1=new Section();
+    public static void getCD(String web) throws Exception {
+        ArrayList<Section> classes = new ArrayList<Section>();
+        Section class1 = new Section();
         String dP = "([0-9]*)\\.([0-9]*)";
-        java.sql.Connection con=getConnection();
-        Connection.Response res=  Jsoup.connect(web).method(Connection.Method.POST).timeout(10*10000)
-            .data("subject","all")
-                .data("display_close","YES")
-                .data("course_type","ALL")
+        java.sql.Connection con = getConnection();
+        Connection.Response res = Jsoup.connect(web).method(Connection.Method.POST).timeout(10 * 10000)
+                .data("subject", "all")
+                .data("display_close", "YES")
+                .data("course_type", "ALL")
                 .followRedirects(true).execute();
 
-        Document doc=res.parse();
+        Document doc = res.parse();
+        Elements detailrows = doc.select("span.course_messages");
+        Elements seats = doc.select("span.course_seats");
+        Elements terms = doc.select("span.course_term");
+        Elements rows = doc.select("tr.list_row");
+        for (Element row : rows) {
+            Elements cells = row.select("td").select("td");
+            PreparedStatement posted = con.prepareStatement("INSERT INTO sections(department,courseId,semester,crn,discipline,courseNumber,section,type,title,hours,beginDate,endDate,url"
+                    + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,)");
+
+        }
     }
 }
 class Section{
-
+    String department;
+   String courseId,semester,crn,discipline,courseNumber,section,type,title,hours,beginDate,endDate,url;
     public Section(){
-
+    department=null;
     }
 
-        }
+    @Override
+    public String toString() {
+        return "Section{" +
+                "department='" + department + '\'' +
+                ", courseId='" + courseId + '\'' +
+                ", semester='" + semester + '\'' +
+                ", crn='" + crn + '\'' +
+                ", discipline='" + discipline + '\'' +
+                ", courseNumber='" + courseNumber + '\'' +
+                ", section='" + section + '\'' +
+                ", type='" + type + '\'' +
+                ", title='" + title + '\'' +
+                ", hours='" + hours + '\'' +
+                ", beginDate='" + beginDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
